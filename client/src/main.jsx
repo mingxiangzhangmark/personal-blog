4409cd4706b28b18c052ba5956c09f88ad1aede3
Mark.Zhang <mingxiangzhang_mark@outlook.com>
2024-12-14 16:56:26 +1100

    change1

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux'
 import { store, persistor } from '../redux/store.js'
 import { PersistGate } from 'redux-persist/integration/react'
-import ThemeProvider from './components/themeProvider.jsx'
+import ThemeProvider from './components/ThemeProvider.jsx'
 
 // createRoot(document.getElementById('root')).render(
 
