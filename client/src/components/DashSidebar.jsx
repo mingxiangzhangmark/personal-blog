1992433e319f1b84fc102888f502682820682a43
Mark.Zhang <mingxiangzhang_mark@outlook.com>
2024-12-17 17:38:56 +1100

    user page for admin user

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,6 +1,6 @@
 import { Sidebar,  } from "flowbite-react";
 import { useEffect, useState } from "react";
-import { HiArrowSmRight, HiDocumentText, HiUser } from "react-icons/hi";
+import { HiArrowSmRight, HiDocumentText, HiUser, HiUsers } from "react-icons/hi";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
 import { signoutSuccess } from "../../redux/user/userSlice";
@@ -49,6 +49,13 @@ export default function DashSidebar() {
             <Link to='/dashboard?tab=posts'>
             <Sidebar.Item  active= {tab === 'posts'} icon = {HiDocumentText}  labelColor = 'dark' as ='div'>
               Posts
+            </Sidebar.Item>
+          </Link>
+          )}
+          {currentUser.isAdmin && (
+            <Link to='/dashboard?tab=users'>
+            <Sidebar.Item  active= {tab === 'users'} icon = {HiUsers}  labelColor = 'dark' as ='div'>
+              Users
             </Sidebar.Item>
           </Link>
           )}
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,6 +3,7 @@ import { useLocation } from "react-router-dom"
 import DashSidebar from "../components/DashSidebar.jsx";
 import  DashProfile  from "../components/DashProfile.jsx";
 import DashPosts from "../components/DashPosts.jsx";
+import DashUsers from "../components/DashUsers.jsx";
 
 export default function Dashboard() {
   const location = useLocation();
@@ -22,6 +23,7 @@ export default function Dashboard() {
       </div>
       {tab === 'profile' && <DashProfile />}
       {tab === 'posts' && <DashPosts />}
+      {tab === 'users' && <DashUsers />}
     </div>
     
   )
